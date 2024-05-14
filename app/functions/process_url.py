836bd8741e56b05fd7857491731a0d0f0054cf62
Filip <szczypulafilip1@gmail.com>
2024-05-14 14:06:23 +0200

    dodane zmienne

--- app/functions/process_url.py
+++ app/functions/process_url.py
@@ -13,6 +13,18 @@ def extract_features(url):
     at_symbol = '@' in url
     tilde_symbol = '~' in url
 
+    num_underscore = url.count('_')
+    num_underscore_in_hostname = parsed_url.netloc.count('_')
+    num_percent = url.count('%')
+    num_query_parts = url.count('&')
+    num_query_params = url.count('?')
+    num_ampersand = url.count('&')
+    num_hash = url.count('#')
+    num_numeric_chars = sum(c.isdigit() for c in url)
+    hostname_length = len(parsed_url.netloc)
+    path_length = len(parsed_url.path)
+    double_slash_in_path = '//' in parsed_url.path
+
     return pd.Series({
         'NumDots': num_dots,
         'SubdomainLevel': subdomain_level,
@@ -21,5 +33,17 @@ def extract_features(url):
         'NumDash': num_dash,
         'NumDashInHostname': num_dash_in_hostname,
         'AtSymbol': at_symbol,
-        'TildeSymbol': tilde_symbol
+        'TildeSymbol': tilde_symbol,
+        'NumUnderscore': num_underscore,
+        'NumUnderscoreInHostname': num_underscore_in_hostname,
+        'NumPercent': num_percent,
+        'NumQueryParts': num_query_parts,
+        'NumQueryComponents': num_query_params,
+        'NumAmpersand': num_ampersand,
+        'NumHash': num_hash,
+        'NumNumericChars': num_numeric_chars,
+        'HostnameLength': hostname_length,
+        'PathLength': path_length,
+        'DoubleSlashInPath': double_slash_in_path
+
     })
